03950e5bb1aad0c5a469807ca88fe02ab1c46413
Admin <Admin@PF2K5L6J-OEW.inc.bytedance.com>
2021-04-30 19:22:44 +0800

    try your ddl

--- class/SecBFS.js
+++ class/SecBFS.js
@@ -1,10 +1,43 @@
 function readfile(loc1,loc2){
-    return json.pares(getfile(`./ticketData/${loc1}_${loc2}.json`))
+    return JSON.parse(getfile(`./ticketData/${loc1}_${loc2}.json`))
 
 }
-function cacuFee(){
+function cacuFee(worker,planarr){
 
 }
-function SecDFS(arr){
-    readline()
+while(line<filelength){
+    let sumplan = JSON.parse(readline('../PlanData.txt'))
+    for(let i=1;i<sumplan.length;i++){
+        SecDFS(workerData[i],sumplan[i])
+    }
+}
+
+
+function SecDFS(worker,plan){
+    
+    let minFee=POSITIVE_INFINITY
+    let minPlan=[]
+    let set = new Set()
+    function dfs2(curArr,set){
+        if(curArr.length===plan.length){
+            let fee = cacuFee(woker,curArr)
+            if(fee && minfee>fee){
+                minfee=fee
+                minPlan=curArr.slice()
+            }
+            return 
+        }
+        for(let i=1;i<plan.length;i++){
+            if(!set.has(i)){
+                set.add(i)
+                curArr.push(i)
+                set.delete(i)
+                curArr.pop()
+            }
+        }
+    }
+    
+}
+for(i in workerData){
+    workerData[i].temp['projects']=SecDFS(workerData[i],plan[i])
 }
